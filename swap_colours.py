5a51629e3ea1784423b19a299d33f538c065927d
Don Kirkby <donkirkby@gmail.com>
2019-12-01 14:45:54 -0800

    Add final photo.

--- swap_colours.py
+++ swap_colours.py
@@ -5,12 +5,13 @@ import cv2
 def main():
     image = cv2.imread('helix.jpg')
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    blue_hue = 105
-    blue_mask = cv2.inRange(hsv, (104, 60, 20), (114, 224, 200))
+    blue_hue = 107
+    blue_mask = cv2.inRange(hsv, (98, 70, 0), (115, 255, 255))
     red_hue = 0
-    red1_mask = cv2.inRange(hsv, (0, 60, 20), (16, 255, 220))
-    red2_mask = cv2.inRange(hsv, (172, 60, 20), (180, 222, 220))
+    red1_mask = cv2.inRange(hsv, (0, 120, 0), (9, 255, 255))
+    red2_mask = cv2.inRange(hsv, (150, 30, 0), (180, 255, 255))
     red_mask = red1_mask | red2_mask
+    # red_mask = cv2.inRange(hsv, (1, 0, 0), (0, 0, 0))
     orange_mask = cv2.inRange(hsv, (110, 60, 20), (16, 255, 220))
     blue_masked = cv2.bitwise_and(hsv, hsv, mask=blue_mask)
     red_masked = cv2.bitwise_and(hsv, hsv, mask=red_mask)
@@ -39,8 +40,10 @@ def main():
     # orange_masked *= 0
 
     masked = red_masked | blue_masked | orange_masked | bg_masked
-    masked = cv2.cvtColor(masked, cv2.COLOR_HSV2RGB)
     masked = cv2.flip(masked, 1)
+    masked = cv2.cvtColor(masked, cv2.COLOR_HSV2BGR)
+    cv2.imwrite('swapped.jpg', masked)
+    masked = cv2.cvtColor(masked, cv2.COLOR_BGR2RGB)
 
     plt.imshow(masked)
     plt.gca().set_axis_off()
